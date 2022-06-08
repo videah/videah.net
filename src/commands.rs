aca7ab447f65cd59f557ff72206dafd6f958ba81
videah <videah@selfish.systems>
2022-06-08 01:57:32 +0100

    remove unused crates accumulated during development

--- src/commands.rs
+++ src/commands.rs
@@ -20,38 +20,11 @@ pub trait Command: Clone {
     fn text(&self) -> &String;
 }
 
-#[derive(Clone)]
-pub struct SocialCommand {
-    command_text: String
-}
-
-impl SocialCommand {
-    pub fn new(command: &str) -> Self {
-        SocialCommand { command_text: command.to_string() }
-    }
-}
-
-impl Command for SocialCommand {
-
-    fn render(&self) -> LazyNodes {
-        rsx! {
-            div{
-                "Hello, World"
-            }
-        }
-    }
-
-    fn text(&self) -> &String {
-        &self.command_text
-    }
-}
-
 pub fn get_command_handler(args: Vec<&str>, command: &str) -> Box<dyn Command> {
     match args[0] {
-        "social" => Box::new(SocialCommand::new(command)),
+        "social" => Box::new(MarkdownCommand::new(command, include_str!("../static/text/social.md"))),
         "intro" => Box::new(HtmlCommand::new(command, include_str!("../static/text/intro.html"))),
         "echo" => Box::new(EchoCommand::new(command)),
-        "gay" => Box::new(MarkdownCommand::new(command, include_str!("../static/text/social.md"))),
         "twitter" => Box::new(RedirectCommand::new(command, "https://twitter.com/videah_")),
         &_ => Box::new(UnknownCommand::new(command)),
     }
